9604e413777fd31e9ddb312c42b01f9e37187e53
Alexey Litvinov <iamhoneyboy@gmail.com>
2015-06-03 00:22:31 +0300

    Подключил плагины

--- bro/modules/optimize.js
+++ bro/modules/optimize.js
@@ -2,9 +2,86 @@
 
 import js2svg from 'svgo/lib/svgo/js2svg';
 import svg2js from 'svgo/lib/svgo/svg2js';
-import plugins from 'svgo/lib/svgo/plugins';
 
-var PLUGINS_LIST = [];
+var PLUGINS_LIST = [
+  require('svgo/plugins/cleanupAttrs'),
+  require('svgo/plugins/removeDoctype'),
+  require('svgo/plugins/removeXMLProcInst'),
+  require('svgo/plugins/removeComments'),
+  require('svgo/plugins/removeMetaData'),
+  require('svgo/plugins/removeDesc'),
+  require('svgo/plugins/removeUselessDefs'),
+  require('svgo/plugins/removeEditorsNSData'),
+  require('svgo/plugins/removeEmptyAttrs'),
+  require('svgo/plugins/removeHiddenElems'),
+  require('svgo/plugins/removeEmptyText'),
+  require('svgo/plugins/removeEmptyContainers'),
+  require('svgo/plugins/convertStyleToAttrs'),
+  require('svgo/plugins/convertColors'),
+  require('svgo/plugins/convertPathData'),
+  require('svgo/plugins/convertTransform'),
+  require('svgo/plugins/removeUnknownsAndDefaults'),
+  require('svgo/plugins/removeNonInheritableGroupAttrs'),
+  require('svgo/plugins/removeUselessStrokeAndFill'),
+  require('svgo/plugins/removeUnusedNS'),
+  require('svgo/plugins/cleanupIDs'),
+  require('svgo/plugins/cleanupNumericValues'),
+  require('svgo/plugins/moveElemsAttrsToGroup'),
+  require('svgo/plugins/moveGroupAttrsToElems'),
+  require('svgo/plugins/collapseGroups'),
+  require('svgo/plugins/convertShapeToPath')
+];
+
+function plugins(ast, list) {
+  list.forEach(function (plugin) {
+    switch(plugin.type) {
+    case 'perItem':
+      ast = perItem(ast, plugin);
+      break;
+    case 'perItemReverse':
+      ast = perItem(ast, plugin, true);
+      break;
+    case 'full':
+      ast = full(ast, plugin);
+      break;
+    }
+  });
+
+  return ast;
+}
+
+function perItem(data, plugin, reverse) {
+  function monkeys(items) {
+    items.content = items.content.filter(function(item) {
+      if (reverse && item.content && item.elem != 'foreignObject') {
+        monkeys(item);
+      }
+
+      var filter = true;
+      if (plugin.active && plugin.fn(item, plugin.params) === false) {
+        filter = false;
+      }
+
+      if (!reverse && item.content && item.elem != 'foreignObject') {
+        monkeys(item);
+      }
+
+      return filter;
+    });
+
+    return items;
+  }
+
+  return monkeys(data);
+}
+
+function full(data, plugin) {
+  if (plugin.active) {
+      data = plugin.fn(data, plugin.params);
+  }
+
+  return data;
+}
 
 export default function optimize(svg, callback) {
   svg2js(svg, (ast) => {
